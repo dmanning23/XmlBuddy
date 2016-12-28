3d1d21891bdbe49e24315f1e6478ba30a3789c82
Dan Manning <dmanning23@hotmail.com>
2016-12-27 22:45:47 -0600

    published nuget

---
+++ XmlBuddy/XmlBuddy.Content.Tests/XmlSourceTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlBuddy.Content.Tests
+{
+	[TestFixture]
+    public class XmlSourceTests
+    {
+		[Test]
+		public void XmlCodeTest()
+		{
+			var test = new XmlSource("test");
+
+			Assert.AreEqual("test", test.XmlCode);
+		}
+    }
+}
